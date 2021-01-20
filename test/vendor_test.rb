07ab393408cdde456ee9dc858daff1a64b74bd4b
Gvieve <gnuebel84@yahoo.com>
2021-01-20 09:51:29 -0700

    Add test for potential_revenue in Vendor

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -45,4 +45,23 @@ class VendorTest < Minitest::Test
 
     assert_equal true, vendor.sell_item?('Peach')
   end
+
+  def test_potential_revenue
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.00"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 100)
+    vendor1.stock(item2, 10)
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 4)
+    vendor2.stock(item3, 20)
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 100)
+
+    assert_equal 80.00, vendor1.potential_revenue
+    assert_equal 117.00, vendor2.potential_revenue
+    assert_equal 75.00, vendor3.potential_revenue
+  end
 end
