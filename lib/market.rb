bfe20c1f580232725ce93671fe7b8e59d60e8c2c
Gvieve <gnuebel84@yahoo.com>
2021-01-20 10:28:58 -0700

    Add tests and code for total_inventory method in Market

--- lib/market.rb
+++ lib/market.rb
@@ -22,4 +22,20 @@ class Market
       vendor.sell_item?(item_name)
     end
   end
+
+  def total_inventory
+    totals = {}
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, qty|
+        if totals[item].nil?
+          totals[item] = {quantity: 0, vendors: []}
+        end
+
+        totals[item][:quantity] += qty
+        totals[item][:vendors].push(vendor)
+      end
+    end
+
+    totals
+  end
 end
